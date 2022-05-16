0e20c0719694d3f563c05866f2fb2988e1ad9787
Jibin <jibinlawrance@gmail.com>
2022-05-17 01:07:13 +0530

    Add auto complete feature while searching for meals name

--- script.js
+++ script.js
@@ -5,11 +5,40 @@ const searchWrapper = document.getElementById('wrapper');
 const resultWrapper = document.getElementById('result');
 
 searchInput.addEventListener('keyup', (e) => {
+    console.log('pressed');
     let results = [];
-    let input = searchInput.value;
-    let trimInput = input.trim();
+    let input = searchInput.value.trim();
 
-    if(trimInput){
-        console.log(input);
+    if(input.length){
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+        .then(res=> res.json())
+        .then(data => {
+            if(data.meals){
+                renderResults(data.meals);
+            }else{
+                console.log('No results');
+                return searchWrapper.classList.remove('show');
+            };
+        })
+    }else{
+        searchWrapper.classList.remove('show');  
     }
+
 });
+
+resultWrapper.addEventListener('click',(e)=>{
+    searchInput.value = e.target.innerText;
+    searchWrapper.classList.remove('show');
+})
+
+function renderResults(results){
+
+    let content = ''
+    for(let key of results){
+        content = content+`<li>${key.strMeal}</li>` ;
+    }
+
+    searchWrapper.classList.add('show');
+    resultWrapper.innerHTML = `<ul>${content}</ul>`
+}
