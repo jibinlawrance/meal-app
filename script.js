e9e287ca03a8313b99308ae7bb716da042e42f5c
jibinlawrance <jibinlawrance@gmail.com>
2022-05-17 17:04:18 +0530

    Add the search button and display the meal on the page

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ let searchList = [ 'Asia', 'Africa', 'North America', 'South America', 'Antarcti
 const searchInput = document.getElementById('search');
 const searchWrapper = document.getElementById('wrapper');
 const resultWrapper = document.getElementById('result');
+const searchMeal = document.getElementById('search-meal');
+const mealWrapper = document.querySelector('.meal-wrapper');
 let resultUl;
+let mealName;
 
 searchInput.addEventListener('keyup', () => {
     let results = [];
     let input = searchInput.value.trim();
+    mealName = '';
+    mealWrapper.innerHTML = '';
 
     if(input.length){
 
@@ -17,9 +22,8 @@ searchInput.addEventListener('keyup', () => {
             if(data.meals){
                 renderResults(data.meals);
             }else{
-                console.log('No results');
+                // console.log('No results');
                 resultWrapper.innerHTML = `<ul class="no-result-ul"><li>No results</li></ul>`
-                // return searchWrapper.classList.remove('show');
             };
         })
     }else{
@@ -28,11 +32,22 @@ searchInput.addEventListener('keyup', () => {
 
 });
 
+searchMeal.addEventListener('click', (e)=>{
+    if(mealName){
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+        .then(res=> res.json())
+        .then(data=>{
+            // console.log(data.meals[0]);
+            mealWrapper.innerHTML = `<h2>${data.meals[0].strMeal}</h2> <img src="${data.meals[0].strMealThumb}" alt="">`
+        })
+    }
+});
 
-function addClick(resultUl){
+function addClickOnLi(resultUl){
     for(let i = 0; i < resultUl.children.length; i++){
         resultUl.children[i].addEventListener('click', (e)=>{
             searchInput.value = e.target.innerText;
+            mealName = searchInput.value;
             searchWrapper.classList.remove('show');
         });
     }
@@ -49,5 +64,5 @@ function renderResults(results){
     resultWrapper.innerHTML = `<ul>${content}</ul>`
 
     resultUl = document.querySelector('#result ul')
-    addClick(resultUl);
+    addClickOnLi(resultUl);
 }
